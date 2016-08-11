50dcf0e919a0d03d5165ba1f1f87386d7f4cf2e4
Stephan Matthai <stephan.matthai@unimelb.edu.au>
2016-08-11 14:49:28 +1000

    chore: add overlooked variable multiplication functions to model library

---
+++ source/modules/model/variableOperations.cpp
@@ -0,0 +1,211 @@
+#include "variableOperations.h"
+
+using namespace std;
+
+namespace csmp {
+
+template<size_t dim>
+Point<dim> operator+(const Point<dim>& p, const VectorVariable<dim>& v ){
+    Point<dim> newp;
+    for (size_t idim = 0; idim <dim;++idim)
+        newp[idim]=p[idim]+v[idim];
+    return std::move(newp);
+}
+
+template<size_t dim>
+Point<dim> operator-(const Point<dim>& p, const VectorVariable<dim>& v ){
+    Point<dim> newp;
+    for (size_t idim = 0; idim <dim;++idim)
+        newp[idim]=p[idim]-v[idim];
+    return std::move(newp);
+}
+
+template<size_t dim>
+Point<dim> operator*(const Point<dim>& p, const VectorVariable<dim>& v ){
+    Point<dim> newp;
+    for (size_t idim = 0; idim <dim;++idim)
+        newp[idim]=p[idim]*v[idim];
+    return std::move(newp);
+}
+
+template<size_t dim>
+Point<dim> operator/(const Point<dim>& p, const VectorVariable<dim>& v ){
+    Point<dim> newp;
+    for (size_t idim = 0; idim <dim;++idim)
+        newp[idim]=p[idim]/v[idim];
+    return std::move(newp);
+}
+
+template<size_t dim >
+double64 dotProduct(const VectorVariable<dim>& v1,  const csmp::Point<dim>& p ) {
+    double64 dprod(0.0);
+    for (size_t i = 0U ; i < dim; ++i)
+        dprod += v1[i]*p[i];
+
+    return dprod;
+}
+
+template<size_t dim>
+double64 dotProduct(  const VectorVariable<dim>& v1,  const VectorVariable<dim>& v2 ) {
+    double64 dprod(0.0);
+    for (size_t i = 0U ; i < dim; ++i)
+        dprod += v1[i] * v2[i];
+    return dprod;
+}
+
+template double64 dotProduct( const VectorVariable<1U>&, const csmp::Point<1U>& ) ;
+template double64 dotProduct( const VectorVariable<2U>&, const csmp::Point<2U>& ) ;
+template double64 dotProduct( const VectorVariable<3U>&, const csmp::Point<3U>& ) ;
+
+template double64 dotProduct( const VectorVariable<1U>&, const VectorVariable<1U>& ) ;
+template double64 dotProduct( const VectorVariable<2U>&, const VectorVariable<2U>& ) ;
+template double64 dotProduct( const VectorVariable<3U>&, const VectorVariable<3U>& ) ;
+
+VectorVariable<1U> crossProduct(  const VectorVariable<1U>& v,  const csmp::Point<1U>& )
+{
+    return std::move(VectorVariable<1U>(v.Flag(),0.0));
+}
+
+VectorVariable<2U> crossProduct(  const VectorVariable<2U>& v1,  const csmp::Point<2U>& p )
+{
+    return std::move(VectorVariable<2U>(v1.Flag(0),v1.Flag(1),0.0,v1[0]*p[1]-v1[1]*p[0]));
+}
+
+VectorVariable<3U> crossProduct(  const VectorVariable<3U>& v1,  const csmp::Point<3U>& p ) {
+
+    return std::move(VectorVariable<3U>(v1.Flag(0),
+                              v1.Flag(1),
+                              v1.Flag(2),(v1[1]*p[2]-v1[2]*p[1]),(v1[2]*p[0] - v1[0]*p[2]),
+                                         (v1[0]*p[1] - v1[1]*p[0])) );
+}
+
+
+VectorVariable<1U> crossProduct(  const VectorVariable<1U>& v1,  const VectorVariable<1U>& v2 ) {
+    return VectorVariable<1U>(v1.Flag(),0.0);
+}
+
+VectorVariable<2U> crossProduct(  const VectorVariable<2U>& v1,  const VectorVariable<2U>& v2 ) {
+    return VectorVariable<2U>(v1.Flag(0),v1.Flag(1),0.0,v1[0]*v2[1]-v1[1]*v2[0]);
+}
+
+VectorVariable<3U> crossProduct(  const VectorVariable<3U>& v1,  const VectorVariable<3U>& v2 ) {
+
+    return std::move(VectorVariable<3U>(v1.Flag(0),
+                              v1.Flag(1),
+                              v1.Flag(2),(v1[1]*v2[2]-v1[2]*v2[1]),(v1[2]*v2[0] - v1[0]*v2[2]),
+                                         (v1[0]*v2[1] - v1[1]*v2[0])));
+}
+
+//here, the flags being kept belong to the vector (legacy).
+//perhaps, because of the multiplication order, the tensor ones should be kept.
+//Nevertheless, I will keep this since the TensorVariable_Test uses this convention
+//Julian Mindel 20.04.2016
+template<size_t dim>
+VectorVariable<dim> multiplyTensorByVector( const TensorVariable<dim>& ts, const VectorVariable<dim>& vc )
+{
+    VectorVariable<dim> resultvec(ANY,0.0);
+
+    for (size_t i = 0U ; i < dim; ++i)
+        for (size_t j = 0U ; j < dim; ++j)
+            resultvec(i)+=ts(i,j) * vc[j];
+
+    for (size_t i = 0U ; i < dim; ++i)
+        resultvec.Flag(i)=ts.Flag(i);
+  
+    return std::move(resultvec);
+}
+
+/// this essentially carries out the same task as MultiplyTensorByVector, but has a meaningful name
+/// emphasizing the fact that only vectors that are horizontal/transposed can be multiplied by tensors.
+template<size_t dim>
+VectorVariable<dim> multiplyHorizontalVectorByTensor( const VectorVariable<dim>& vc, const TensorVariable<dim>& ts )
+{
+    VectorVariable<dim> resultvec(ANY,0.0);
+
+    for (size_t i = 0U ; i < dim; ++i)
+        for (size_t j = 0U ; j < dim; ++j)
+            resultvec(i)+=vc[j] * ts(j,i);
+
+    for (size_t i = 0U ; i < dim; ++i)
+        resultvec.Flag(i)=vc.Flag(i);
+
+    return std::move(resultvec);
+}
+
+template<size_t dim>
+TensorVariable<dim> multiplyTensorByTensor( const TensorVariable<dim>& ts, const TensorVariable<dim>& ts2 )
+{
+    TensorVariable<dim> resulttensor(ANY,0.0);
+
+    for (size_t i = 0U ; i < dim; ++i)
+        for (size_t j = 0U ; j < dim; ++j)
+            for (size_t k = 0U ; k < dim; ++k)
+                resulttensor(i,j)+=ts(i,k) * ts2(k,j);
+    for (size_t i = 0U ; i < dim; ++i)
+        resulttensor.Flag(i)=ts.Flag(i);
+
+    return std::move(resulttensor);
+}
+
+/// return angle in degrees
+/// this function was taken from an old one called AngleTo, and templetized
+template<size_t dim>
+double64  angleBetween( const VectorVariable<dim>& v1, const VectorVariable<dim>& v2 )
+{
+    double64 ab, a_dot_b;
+
+    // a b
+    // ---
+    ab      = dotProduct(v1,v2);
+    // |a| . |b|
+    // ---------
+    a_dot_b = v1.Length()*v2.Length();
+
+    // a b
+    // ---
+    double64 cos_angle = ab / a_dot_b;
+
+    // if zero intercept
+    if ( cos_angle == 0.0 ) return  90.0;
+    // if outside of range of 'acos' function
+    if ( cos_angle >  1.0 ) return   0.0;
+    if ( cos_angle < -1.0 ) return 180.0;
+
+    return (180.0/3.14159265358979324) * std::acos(cos_angle);
+}
+
+
+template Point<1U> operator+(const Point<1U>& p, const VectorVariable<1U>& v );
+template Point<2U> operator+(const Point<2U>& p, const VectorVariable<2U>& v );
+template Point<3U> operator+(const Point<3U>& p, const VectorVariable<3U>& v );
+
+template Point<1U> operator-(const Point<1U>& p, const VectorVariable<1U>& v );// found these mostly in VectorVariable_Tests
+template Point<2U> operator-(const Point<2U>& p, const VectorVariable<2U>& v );// found these mostly in VectorVariable_Tests
+template Point<3U> operator-(const Point<3U>& p, const VectorVariable<3U>& v );// found these mostly in VectorVariable_Tests
+
+template Point<1U> operator*(const Point<1U>& p, const VectorVariable<1U>& v );// found these mostly in VectorVariable_Tests
+template Point<2U> operator*(const Point<2U>& p, const VectorVariable<2U>& v );// found these mostly in VectorVariable_Tests
+template Point<3U> operator*(const Point<3U>& p, const VectorVariable<3U>& v );// found these mostly in VectorVariable_Tests
+
+template Point<1U> operator/(const Point<1U>& p, const VectorVariable<1U>& v );// found these mostly in VectorVariable_Tests
+template Point<2U> operator/(const Point<2U>& p, const VectorVariable<2U>& v );// found these mostly in VectorVariable_Tests
+template Point<3U> operator/(const Point<3U>& p, const VectorVariable<3U>& v );// found these mostly in VectorVariable_Tests
+
+template VectorVariable<1U> multiplyTensorByVector( const TensorVariable<1U>&, const VectorVariable<1U>& ) ;
+template VectorVariable<2U> multiplyTensorByVector( const TensorVariable<2U>&, const VectorVariable<2U>& ) ;
+template VectorVariable<3U> multiplyTensorByVector( const TensorVariable<3U>&, const VectorVariable<3U>& ) ;
+
+template VectorVariable<1U> multiplyHorizontalVectorByTensor( const VectorVariable<1U>&, const TensorVariable<1U>&  ) ;
+template VectorVariable<2U> multiplyHorizontalVectorByTensor( const VectorVariable<2U>&, const TensorVariable<2U>&  ) ;
+template VectorVariable<3U> multiplyHorizontalVectorByTensor( const VectorVariable<3U>&, const TensorVariable<3U>&  ) ;
+
+template TensorVariable<1U> multiplyTensorByTensor( const TensorVariable<1U>&, const TensorVariable<1U>& ) ;
+template TensorVariable<2U> multiplyTensorByTensor( const TensorVariable<2U>&, const TensorVariable<2U>& ) ;
+template TensorVariable<3U> multiplyTensorByTensor( const TensorVariable<3U>&, const TensorVariable<3U>& ) ;
+
+template double64  angleBetween( const VectorVariable<1U> & v1, const VectorVariable<1U>& v2 );
+template double64  angleBetween( const VectorVariable<2U> & v1, const VectorVariable<2U>& v2 );
+template double64  angleBetween( const VectorVariable<3U> & v1, const VectorVariable<3U>& v2 );
+
+} // end namespace csmp
+
