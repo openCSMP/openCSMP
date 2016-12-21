36332bdc6b316d16954a99c644e6095b472ce003
Junchul Kim <junchulk@student.unimelb.edu.au>
2016-12-21 12:48:15 +1100

    modified the build script for generating documentation from the CSMP sources

--- build/windows/make_msvc.bat
+++ build/windows/make_msvc.bat
@@ -1,3 +1,9 @@
+doxygen "C:\CSMP\CSMP_DOxygen\Doxyfile"
+tar -cvf docbook.tar -C "C:\CSMP\CSMP_Documentation" docbook
+tar -cvf html.tar -C "C:\CSMP\CSMP_Documentation" html
+tar -cvf latex.tar -C "C:\CSMP\CSMP_Documentation" latex
+tar -cvf man.tar -C "C:\CSMP\CSMP_Documentation" man
+
 if exist .\CSMP_number_types.h (
 	bjam --toolset=msvc-14.0 --build-type=complete --layout=system optimization=speed address-model=64 link=static threading=multi 
 ) else (
