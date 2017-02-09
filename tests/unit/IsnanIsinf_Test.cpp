85839bacf57bc0a79c125c746df6abd1b8470c6f
Andrew Bromage <abromage@unimelb.edu.au>
2017-02-09 16:47:48 +1100

    bug: Fixing IEEE-754 tests
    isnan(), isinf() etc only guarantee to return a true value
    if they are true, not necessarily 1.

--- tests/unit/IsnanIsinf_Test.cpp
+++ tests/unit/IsnanIsinf_Test.cpp
@@ -19,43 +19,43 @@ void IsnanIsinf_Test::run()
 
 	// isnan tests
 	getInfoStream() << "isnan(0.0) = "	<< isnan(0.0);
-	_test( isnan(0.0) == 0 );
+	_test( !isnan(0.0) );
 
 	getInfoStream() << "isnan( 1.0/0.0 ) = "<< isnan(1.0/zero);
-	_test( isnan(1.0/zero) == 0 );
+	_test( !isnan(1.0/zero) );
 
 	getInfoStream() << "isnan( -1.0/0.0 ) = "<< isnan(-1.0/zero);
-	_test( isnan(-1.0/zero) == 0 );
+	_test( !isnan(-1.0/zero) );
 
 	getInfoStream() << "isnan( sqrt(-1.0) ) = "<< isnan(sqrt(-1.0));
-	_test( isnan(sqrt(-1.0)) == 1 );
+	_test( isnan(sqrt(-1.0)) );
 
 	// isinf tests
 
 	getInfoStream() << "isinf( 0.0 ) = "<< isinf(0.0);
-	_test( isinf(0.0) == 0 );
+	_test( !isinf(0.0) );
 
 	getInfoStream() << "isinf( 1.0/0.0 ) = "<< isinf(1.0/zero);
-	_test( isinf(1.0/zero) == 1 );
+	_test( isinf(1.0/zero) );
 
 	getInfoStream() << "isinf( -1.0/0.0 ) = "<< isinf(-1.0/zero);
-	_test( isinf(-1.0/zero) == 1 );
+	_test( isinf(-1.0/zero) );
 
 	getInfoStream() << "isinf( sqrt(-1.0) ) = "<< isinf(sqrt(-1.0));
-	_test( isinf(sqrt(-1.0)) == 0 );
+	_test( !isinf(sqrt(-1.0)) );
 
 	// isfinite tests
 	getInfoStream() << "isfinite( 0.0 ) = "<< isfinite(0.0);
-	_test( isfinite(0.0) == 1 );
+	_test( isfinite(0.0) );
 
 	getInfoStream() << "isfinite( 1.0/0.0 ) = "<< isfinite(1.0/zero);
-	_test( isfinite(1.0/zero) == 0 );
+	_test( !isfinite(1.0/zero) );
 
 	getInfoStream() << "isfinite( -1.0/0.0 ) = "<< isfinite(-1.0/zero);
-	_test( isfinite(-1.0/zero) == 0 );
+	_test( !isfinite(-1.0/zero) );
 
 	getInfoStream() << "isfinite( sqrt(-1.0) ) = "	<< isfinite(sqrt(-1.0));
-	_test( isfinite(sqrt(-1.0)) == 0 );
+	_test( !isfinite(sqrt(-1.0)) );
 }
 
 
