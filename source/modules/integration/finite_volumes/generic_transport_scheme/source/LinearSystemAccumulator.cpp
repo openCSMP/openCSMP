7d3b10489912872eddf7d63067ca1bf23bd98812
Andrew Bromage <abromage@unimelb.edu.au>
2017-12-06 17:14:13 +1100

    LinearSystemAccumulator now accumulates

--- source/modules/integration/finite_volumes/generic_transport_scheme/source/LinearSystemAccumulator.cpp
+++ source/modules/integration/finite_volumes/generic_transport_scheme/source/LinearSystemAccumulator.cpp
@@ -15,10 +15,6 @@
 #include "LinearSolver.h"
 #ifdef CSMP_WITH_SAMG_SOLVER
 #include "SAMG_Settings.h"
-#include "SAMG_Solver.h"
-#else
-#include "GaussJordan_Solver.h"
-#include "Meschach_Solver.h"
 #endif
 
 using namespace std;
@@ -85,12 +81,54 @@ void LinearSystemAccumulator<dim>::FinaliseOperators()
 template<size_t dim>
 void LinearSystemAccumulator<dim>::AccumulateByStencil()
 {
+  auto interior_elmts_end = gref_.InteriorElementsEnd();
+  for (auto eit = gref_.InteriorElementsBegin(); eit != interior_elmts_end; ++eit) {
+    for (auto lhs: interior_lhs_) {
+      lhs->AccumulateStencil( **eit, LHS_ );
+    }
+
+    for (auto rhs: interior_rhs_) {
+      rhs->AccumulateStencil( **eit, RHS_ );
+    }
+  }
+  
+  auto perimeter_elmts_end = gref_.PerimeterElementsEnd();
+  for (auto eit = gref_.PerimeterElementsBegin(); eit != perimeter_elmts_end; ++eit) {
+    for (auto lhs: perimeter_lhs_) {
+      lhs->AccumulateStencil( **eit, LHS_ );
+    }
+    
+    for (auto rhs: perimeter_rhs_) {
+      rhs->AccumulateStencil( **eit, RHS_ );
+    }
+  }
 }
 
 
 template<size_t dim>
 void LinearSystemAccumulator<dim>::AccumulateByFiniteVolume()
 {
+  auto interior_nodes_end = gref_.InteriorNodesEnd();
+  for (auto nit = gref_.InteriorNodesBegin(); nit != interior_nodes_end; ++nit) {
+    for (auto lhs: interior_lhs_) {
+      lhs->AccumulateFiniteVolume( **nit, LHS_ );
+    }
+    
+    for (auto rhs: interior_rhs_) {
+      rhs->AccumulateFiniteVolume( **nit, RHS_ );
+    }
+  }
+  
+  auto perimeter_nodes_end = gref_.PerimeterNodesEnd();
+  for (auto nit = gref_.PerimeterNodesBegin(); nit != perimeter_nodes_end; ++nit) {
+    for (auto lhs: perimeter_lhs_) {
+      lhs->AccumulateFiniteVolume( **nit, LHS_ );
+    }
+    
+    for (auto rhs: perimeter_rhs_) {
+      rhs->AccumulateFiniteVolume( **nit, RHS_ );
+    }
+  }
 }
 
   
