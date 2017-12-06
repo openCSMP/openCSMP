23ac66c028d5106fd3f38276b94bdf15862f295d
Hani <haniakbari@gmail.com>
2017-12-06 16:18:12 +1100

    Edited poreVolume.cpp

--- source/modules/integration/finite_volumes/generic_transport_scheme/source/poreVolume.cpp
+++ source/modules/integration/finite_volumes/generic_transport_scheme/source/poreVolume.cpp
@@ -4,17 +4,18 @@ using namespace std;
 
 namespace csmp {
 
-template<size_t dim>
-PoreVolume<dim>::PoreVolume(Model<dim>& model, const char* porevolume, double64 delta_t)
- : MatrixOperator<dim> (ADD_ACCUMULATE), pv_key_(model.Database().StorageKey(porevolume), dt_(delta_t))
-{
+	template<size_t dim>
+		PoreVolume<dim>::PoreVolume(Model<dim>& model, const char* porevolume, double64 delta_t)
+			: MatrixOperator<dim>(ADD_ACCUMULATE), pv_key_(model.Database().StorageKey(porevolume), dt_(delta_t))
+		{
+
+		}
+
+		template<size_t dim>
+		void AccumulateFV(Node<dim> *nptr, SparseMatrix &A) {
+			double64 pv = nptr->Read(pv_key_);
+			size_t i = nptr->Idx();
+			A(i, i) = pv / dt_;
+		}
 	
-}
-
-PoreVolume<dim>::AccumulateFV(Node<dim> *nptr, SparseMatrix &A){
-	double64 pv = nptr->Read(pv_key_);
-	size_t i = nptr->Idx();
-	A(i,i) = pv/dt_;
-}
-
 } // end csmp
