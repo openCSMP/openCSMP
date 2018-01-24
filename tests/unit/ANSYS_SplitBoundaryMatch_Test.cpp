37bf5b32a84dbc42d168b666523ba1cee641ef8b
Stephan Matthai <stephan.matthai@unimelb.edu.au>
2018-01-24 21:59:02 +1100

    debug: finished new method that outputs the perimeter surface of a 3D region to a VTK file

--- tests/unit/ANSYS_SplitBoundaryMatch_Test.cpp
+++ tests/unit/ANSYS_SplitBoundaryMatch_Test.cpp
@@ -5,6 +5,7 @@
 #include "PL_Utilities.h"
 #include "VTK_Interface.h"
 #include "variableOperations.h"
+#include "CSMP_mathUtilities.h"
 
 #include "Model.h"
 #include "VTU_Interface.h"
@@ -14,6 +15,9 @@
 using namespace std;
 
 namespace csmp {
+
+/// using the boundary faces of a region, the method creates a (triangulated) surface and writes it into a VTK file
+void perimeterSurfaceToVTK( const Model<3U>&,  const char* region );
 
 /**
     tests findSplitInterfaceElements() and the creation of a SplitBoundary from ANSYS
@@ -34,6 +38,7 @@ void ANSYS_SplitBoundaryMatch_Test::run()
       //                          fileset       regions-file
 //      ANSYS_Model3D model( "Model_Split_wall", "Model_Split_wall", variablesFile.c_str(), true );
       ANSYS_Model3D model( "NotSplitWall", "NotSplitWall", variablesFile.c_str(), true );
+      Region<3U> model_domain(model.Region("Model"));
 
     // 0. visualising the model and its regions
     // ----------------------------------------
@@ -45,11 +50,23 @@ void ANSYS_SplitBoundaryMatch_Test::run()
           evar += 1.;
        }
      cerr << endl;
+     // painting all perimeter elements red
+     const csmp::Index evar_key(model.Database().StorageKey("element variable"));
+     for ( auto eit=model_domain.PerimeterElementsBegin(); eit!=model_domain.ElementsEnd(); ++eit )
+       (*eit)->Store( evar_key, makeScalar(PLAIN,5.) );
+
+     // regularising node positions
+    cerr <<"\nrun: focusing on inner box region that has "<< model.Region("INNER_BOX_VOL").Nodes() <<" perimeter nodes.\n";
+    const double n_dec_places(1.0e-3);
+    for ( auto nit=model_domain.NodesBegin(); nit!=model_domain.NodesEnd(); ++nit ) {
+         (*nit)->x( quantiseToScale( (*nit)->x(), n_dec_places ) );
+         (*nit)->y( quantiseToScale( (*nit)->y(), n_dec_places ) );
+         (*nit)->z( quantiseToScale( (*nit)->z(), n_dec_places ) );
+      }
   
      // nodes at model perimeter
      const csmp::Index nvar_key(model.Database().StorageKey("nodal variable"));
      multiset<Point<3U> > split_nodes;
-     Region<3U> model_domain(model.Region("Model"));
      model_domain.InputPropertyValue( "nodal variable", makeScalar(PLAIN,0.) );
      for ( auto nit=model_domain.PerimeterNodesBegin(); nit!=model_domain.NodesEnd(); ++nit ) {
           if ( (*nit)->AtBoundary() != NOT and (*nit)->AtBoundary() != REGION_BOUNDARY )
@@ -62,16 +79,14 @@ void ANSYS_SplitBoundaryMatch_Test::run()
      cerr <<"\nrun: points on ANSYS split boundary:\n";
      size_t counter(0);
      for (auto pt=split_nodes.begin(); pt!=split_nodes.end(); ++pt )
-       cerr << fixed << setprecision(3.) <<"\n\t"<< counter++ <<": "<< (*pt)[0] <<" "<< (*pt)[1] <<" "<< (*pt)[2];
+       cerr << fixed << setprecision(3) <<"\n\t"<< counter++ <<": "<< (*pt)[0] <<" "<< (*pt)[1] <<" "<< (*pt)[2];
   
      VTK_Interface<3U>  vtk_output;
-     vtk_output.OutputDataToVTK(model, "test-evar", "element variable", 0U );
-     vtk_output.OutputDataToVTK(model, "test-nvar", "nodal variable", 0U );
+     vtk_output.OutputDataToVTK( model, "test-evar", "element variable", 0U );
+     vtk_output.OutputDataToVTK( model, "test-nvar", "nodal variable", 0U );
+     // output of perimeter elements to VTK
+     outputRegionBoundaryToVTK( model, "Model", "model-boundary" );
   
-  
-    // 1. trying to manipulate coordinates so that they are identical within a given tolerance
-    // ---------------------------------------------------------------------------------------
-
 
     // 1. create element correspondance set
     // ---------------------------------------------------------------------------------------
@@ -86,6 +101,13 @@ void ANSYS_SplitBoundaryMatch_Test::run()
       const double correspondanceIdentificionTime( timer.getElapsedMilliseconds() );
       cerr <<"\n::run: time taken to identify boundary elements: "<< correspondanceIdentificionTime <<"\n";
   
+      // changing element property values in the discovered elements
+      for ( auto eit=interface_elmt_pairs.begin(); eit!=interface_elmt_pairs.end(); ++eit ) {
+           (*eit).first.first->Store( evar_key, makeScalar(PLAIN,6.) );
+           (*eit).second.first->Store( evar_key, makeScalar(PLAIN,7.) );
+        }
+      vtk_output.OutputDataToVTK(model, "test-evar", "element variable", 1U );
+
 
    // 2. build CSMP SplitBoundary
    // ---------------------------------------------------------------------------------------
