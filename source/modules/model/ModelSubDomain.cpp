76ec80b7a7e8251ddeb3cf90baad580144838717
Andrew Bromage <abromage@unimelb.edu.au>
2018-03-01 16:25:39 +1100

    Fix the "no interior elements" diagnostic
    	Do not under any circumstances look at this fix. It's
    	too embarrassing for me.

--- source/modules/model/ModelSubDomain.cpp
+++ source/modules/model/ModelSubDomain.cpp
@@ -5500,7 +5500,7 @@ void readDomainIndexesFromBinaryFile( size_t dim, FILE* fp, SubDomainInfo& info 
    
     // 2. reading the interior element records of the region
     skm_C_fread( fp, info.interior_elmts );
-    if (dim > 2 && !info.interior_elmts.empty() ) {
+    if (dim > 2 && info.interior_elmts.empty() ) {
         csmp_error.notice( ERROR, "readDomainIndexesFromBinaryFile",
                 "Model appears to have a region with no interior elements: ", name);
     }
