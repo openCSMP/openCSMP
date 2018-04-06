8fc42cff960441b8aabaeb7941dd50173eb21f20
Qi <qishao@fox.geocomp.uq.edu.au>
2018-04-06 14:44:23 +1000

    Changes to make CO2 sequenstation simulator work properly.

--- source/includes/integration/finite_volumes/DES_transport_scheme/TwoPhaseDESTransport.h
+++ source/includes/integration/finite_volumes/DES_transport_scheme/TwoPhaseDESTransport.h
@@ -7,6 +7,7 @@
 #include "Event.h"
 #include "Variables_TwoPhaseFlow.h"
 #include "FlowFunctions.h"
+#include "FibonacciHeap.h"
 
 
 namespace csmp {
@@ -24,6 +25,8 @@ class TwoPhaseDESTransport : public variables::Variables_TwoPhaseFlow {
     TwoPhaseDESTransport ( Model<dim>& m, const char* target_region, FlowFunctions<dim>& flowfunctions, bool with_capillary_spreading, bool with_gravity_forces);
     void AdvectVariable_DES_serial( double64 model_time, double64 cfl_multiplication_factor, double64 PEP_parameter );
     void AdvectVariable_TDS( double64 time_interval, double64 cfl_multiplication_factor, double64 PEP_parameter );
+    
+    typedef ajb::detail::FibonacciHeap_Node<double64,size_t> Heap_Node;
 
   private:
     void initializeVariablsAndKeys(Model<dim>& m);
@@ -32,16 +35,19 @@ class TwoPhaseDESTransport : public variables::Variables_TwoPhaseFlow {
     bool Schedule(Event<dim>* nd, double64 t_end, double64 cfl_multiplier);
     void Update_DES(Event<dim>* nd, double64 t_clock);
     void Update_TDS(Event<dim>* nd, double64 delta_t);
-    void Synchronize(Event<dim>* nd,double64 t_clock);
+    void Synchronize(Event<dim>* nd,double64 t_clock,double64& t_remove);
 
     Region<dim>& gref_;
     FlowFunctions<dim>& flowfunctions_;
     bool with_capillary_spreading_, with_gravity_forces_;
     double64 upper_limit_, lower_limit_; ///< range in which the result is allowed to vary
-    std::vector<Event<dim>*>	PEPStack, EntireQueue, Queue;
+    std::vector<Event<dim>*> PEPList, FullList;
+    std::vector<Heap_Node*> HeapNodeFullList; 
+    ajb::FibonacciHeap<double64,size_t> EventHeap;
     size_t	rate_count_;
     size_t	update_count_;
-    double64 T_RateOfChange_, T_Schedule_, T_SortQueue_, T_Update_, T_Synchronize_, T_RemoveFromQueue_, T_AdvectVariable_; //time recordings
+    double64 T_RateOfChange_, T_Schedule_, T_InsertToHeap_, T_Update_, T_Synchronize_, T_RemoveFromHeap_, T_AdvectVariable_; //time recordings
+    bool 	first_step_;
     
     csmp::INDEX<SCALAR,NODE> key_dsnw, key_EventIndex, key_update, key_rate, key_schedule, key_synchronize;
         
@@ -55,19 +61,7 @@ class TwoPhaseDESTransport : public variables::Variables_TwoPhaseFlow {
     csmp::INDEX<ARRAY,NODE> key_time;
      
 };
-
-
-//class function for sorting event queue based on scheduled time stamps
-template<size_t dim>
-class sort_queue
-{
-    public:
-    sort_queue() {}
-    bool operator()(const Event<dim>* lhs, const Event<dim>* rhs)
-    {
-      return lhs->t_schedule() < rhs->t_schedule(); //scheduled time stamps
-    }
-};    
+  
 
 }//end csmp
 
