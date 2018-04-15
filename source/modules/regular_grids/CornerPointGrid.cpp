65082dd49cbc514b23c406b83ec329ded017e97f
Andrew Bromage <abromage@unimelb.edu.au>
2018-04-15 17:27:17 +1000

    TOP_FLAG and BOTTOM_FLAG were swapped in RegularPointGrid

--- source/modules/regular_grids/CornerPointGrid.cpp
+++ source/modules/regular_grids/CornerPointGrid.cpp
@@ -1198,16 +1198,16 @@ void CornerPointGrid<dim>
               node_flags[6] |= IRREGULAR_FLAG;
             }
             if ( k == kmax ) {
-              node_flags[4] |= TOP_FLAG;
-              node_flags[5] |= TOP_FLAG;
-              node_flags[6] |= TOP_FLAG;
-              node_flags[7] |= TOP_FLAG;
-            }
-            if ( k == kmin ) {
               node_flags[0] |= BOTTOM_FLAG;
               node_flags[1] |= BOTTOM_FLAG;
               node_flags[2] |= BOTTOM_FLAG;
               node_flags[3] |= BOTTOM_FLAG;
+            }
+            if ( k == kmin ) {
+              node_flags[4] |= TOP_FLAG;
+              node_flags[5] |= TOP_FLAG;
+              node_flags[6] |= TOP_FLAG;
+              node_flags[7] |= TOP_FLAG;
             }
 
           auto& polygon = poly[ hexa_cell_id ];
