31e2b79d578fa8952572e7177f77b26449ac8c1f
StephanMatthai_laptop_87_Pasco <stephan.matthai@gmail.com>
2019-01-04 10:43:51 +1100

    chore: added new function that Thomas needed for his phase state calculations

--- source/modules/eos/h2o_nacl/HaliteLiquidus.cpp
+++ source/modules/eos/h2o_nacl/HaliteLiquidus.cpp
@@ -47,6 +47,11 @@ namespace csmp
     return XNaCl2Massfraction( molefraction_nacl_ );
   }
 
+  double64 HaliteLiquidus::MoleFractionNaCl()
+  {
+      CheckState();
+      return molefraction_nacl_;
+  }
 
   /** Update internal variables for current temperature and pressure
    */
