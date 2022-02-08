8f3b52e48ad62174b86bbb420f7f97d3741e30f2
Anne-Laure Tertois <annelaure.tertois@unimelb.edu.au>
2022-02-08 17:45:59 +0100

    Minor GNU compilation fixes.

--- source/modules/integration/pde_operators/PointSource_lhsop.cpp
+++ source/modules/integration/pde_operators/PointSource_lhsop.cpp
@@ -42,7 +42,7 @@ void PointSource_lhsop<dim,SIMPLEX>::GetOperands( SIMPLEX& e )
        
       // taking into account that the point source contributes to several elements
       for ( size_t i=0U; i<e.Nodes(); i++ )
-        SRC_[i]() /= static_cast<double64>(e.N(i)->Parents());
+        SRC_[i]() /= static_cast<double>(e.N(i)->Parents());
         
    } // end GetOperands
 
