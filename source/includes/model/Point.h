6991581ad57c5813323b0737d38cdd959b52e0cb
Stephan Matthai <stephan.matthai@unimelb.edu.au>
2022-08-18 22:41:24 +0200

    development: created draft of a new pde operator that will allow coupling of domains that are separated by split boundaries

--- source/includes/model/Point.h
+++ source/includes/model/Point.h
@@ -51,10 +51,10 @@ class Point {
     Point& operator/=( double );
   
     /// accessor and mutator of point (0=x coordinate, 1=y...)
-    double& operator[]( uint32_t);
+    double& operator[]( uint32_t );
   
     /// accessor of point (0=x coordinate, 1=y...)
-    const double& operator[]( uint32_t) const;
+    const double& operator[]( uint32_t ) const;
   
     /// compares points using epsilon from numeric_limits
     bool operator==( const Point& ) const;
