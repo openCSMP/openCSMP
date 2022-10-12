45b2f3f22a4c25264c06c39b00cd6ba136355d41
Edoardo Pezzulli <edoardo.pezzulli@erdw.ethz.ch>
2022-10-12 14:45:25 +0200

    Ported over CVFEM Test 1D. With config files in the dataset folder and CVFEM_1D_VVCase test created ready to run. Files still need to be ported to the includes directory for them to be found.

---
+++ tests/integration/CVFEM_1D_VVCase.h
@@ -0,0 +1,106 @@
+#ifndef CVFEM_1D_VVCASE
+#define CVFEM_1D_VVCASE
+
+
+#include "Test.h"
+
+
+// model
+#include "Model1D.h"
+
+// File I/O and Initialization
+#include "ComputationalSettings.h"
+#include "InputDataManager.h"
+
+// finite elements
+#include "NumIntegral_dNT_op_dN_dV.h"
+#include "NumIntegral_dNT_op_dV.h"
+#include "VelocityAndVolumeFlux.h"
+#include "NumIntegral_SetRHS_to_Zero.h"
+
+// finite volumes
+// CVFEM PHX
+#include "CVFEM_MathOperatorRHS.h"
+#include "CVFEM_PHX_Scheme.h"
+
+// visitors
+#include "ConductivityVisitor.h"
+#include "ComputeGravityTermVisitor.h"
+//#include "GEMS3K_Visitor.h"
+
+// eos
+
+// Solver
+#include "LUdcmp_Solver.h"
+#include "EigenSolver.h"
+
+// Output
+#include "VTU_Interface.h"
+#include <time.h>
+
+#include "iostream"
+
+
+using namespace std;
+using namespace csmp;
+
+
+namespace csmp
+  {
+
+  /** CVFEM_1D_VVCase : 1D Test Case
+  =================================
+  Mesh:       Not needed
+  Model:      1D
+  Test:       PHX_Scheme
+  BC:         TBC
+  Criterion:
+  =================================
+  */
+class CVFEM_1D_VVCase : public Test
+  {
+    public:
+          CVFEM_1D_VVCase ( const char* prefix );
+          ~CVFEM_1D_VVCase();
+          void run();
+    private:
+
+          const char* geometry_name_;
+          const char* config_file_name_;
+          const char* vars_name_;
+          const char* system_file_list_name_;
+          const char* recipes_file_list_name_;
+          list<string> output_props_;
+
+          Model<1U>* model;
+          PropertyDatabase<1U>*  pd_ref;
+          VTU_Interface<1U>* vtu;
+          ComputationalSettings  run_settings;
+
+          //! CVFEM instance
+          CVFEM_PHX_Scheme<1U> *CVFEM_PHX;
+
+          //! Chemical diffusion operators and variables
+          std::vector<PDE_Integrator<1U, Region>*>                  diffusion;
+          std::vector<NumIntegral_NT_lhsop_N_dV<1U,Element<1U>>*>  diff_cap_lhs;
+          std::vector<NumIntegral_NT_op_N_dV<1U,Element<1U>>*>     diff_cap_rhs;
+          std::vector<NumIntegral_dNT_op_dN_dV<1U, Element<1U>>*>  diffusivity;
+          Index pore_diff_key, eff_diff_key, porosity_key;
+          ScalarVariable diff_coeff, porosity;
+
+          //! time variables
+          double model_time, dt, max_time, vtu_dt;
+          size_t timestep, output_counter;
+
+          int CalculateMassConductivity();
+          int CalculateMassGravityTerm();
+          int CalculateSteadyStatePressure();
+          int CalculateSteadyStateTemperature();
+          int InitializeFluidPropertiesLinearPressure();
+          int InitializeFluidProperties();
+
+  };
+
+  } // csmp
+
+#endif // CVFEM_1D_VVCase
