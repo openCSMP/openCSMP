5dff28fc2fc9134041d130cbb70dd7f865733df1
Anne-Laure Tertois <annelaure.tertois@unimelb.edu.au>
2023-01-27 05:18:40 +0100

    - added method to record multiple literal choices (record a whole line of text    rather than stopping at the first blank space  - fixed yes / no method in case user entered neither 'y' nor 'n'  - added a third option to yes / no method to display a help message

--- source/includes/run_control/Standard_IO_Handler.h
+++ source/includes/run_control/Standard_IO_Handler.h
@@ -19,7 +19,7 @@ class Standard_IO_Handler {
     ~Standard_IO_Handler();
   
     /// prints question onto screen and collects input from user (stdin)
-    bool        YesNo( const std::string& question );
+    bool        YesNo( const std::string& question, const char* help_message = nullptr );
   
     /// prompts user to enter a floating point value which is collected from stdin returned
     double    RecordChoice( const char* question );
@@ -30,8 +30,11 @@ class Standard_IO_Handler {
     /// prompts user to make a binary choice which is collected from stdin returned
     bool        RecordLogicalChoice( const char* question );
   
-    /// prompts user to input a string of text in response to the posed question
+    /// prompts user to input a word of text in response to the posed question
     std::string RecordLiteralChoice( const char* question );
+  
+    /// prompts user to input a string of text in response to the posed question
+    std::string RecordMultipleLiteralChoice( const char* question );
   
     /// collects text from stdin and stores it in internal container of strings
     void        RecordInformation( const std::string& info );
@@ -48,9 +51,13 @@ class Standard_IO_Handler {
     /// prints all the recorded information to the output text file 'doc_file'
     void        Out() const;
     
+    /// sets default help message fot this handler
+    void        SetDefaultHelp( const std::string& default_help );
+
   private:
     std::string             protocol_file_;  ///< the ASCII text file to which the log will be written
     std::list<std::string>  input_output;    ///< the collected information
+    std::string             default_help_{ "No available help..." }; /// default help message if none is provided
 };
 
 
